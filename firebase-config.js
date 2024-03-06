e898a3a25a78d13bfe249574fa1f26118eb014cb
IIIRN <95156967+IIIRN@users.noreply.github.com>
2024-03-06 17:14:35 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+  // Firebase Configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBEfZKPxqf75KYyi9AWnSwTEoXB_CUIGp0",
+          authDomain: "iiirn-a9b4c.firebaseapp.com",
+          databaseURL: "https://iiirn-a9b4c-default-rtdb.firebaseio.com",
+          projectId: "iiirn-a9b4c",
+          storageBucket: "iiirn-a9b4c.appspot.com",
+          messagingSenderId: "453527572957",
+          appId: "1:453527572957:web:5a28039533fbef81a8e634",
+          measurementId: "G-YLM6GQD6Y0"
+  };
+
+  // Initialize Firebase
+
+  firebase.initializeApp(firebaseConfig);
+
+
